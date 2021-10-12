bad5091103901d95910fc50dc0b7b6ae4db03018
cyh <cccchenyuhao@gmail.com>
2021-10-12 17:48:13 +0800

    update

--- server/src/resource/user/user.controller.js
+++ server/src/resource/user/user.controller.js
@@ -1,47 +1,51 @@
-const User = require('./user.model')
+const User = require("./user.model");
 
 async function getAllUsers(req, res) {
   try {
-    const users = await User.find()
-    res.status(200).json(users)
+    const users = await User.find();
+    res.status(200).json(users);
   } catch (error) {
-    console.error(error)
-    res.status(400).json({message: error.message})
+    console.error(error);
+    res.status(400).json({ message: error.message });
   }
 }
 
 async function getUserById(req, res) {
   try {
-    const users = await User.find({_id: req.params.id})
-    res.status(200).json(users)
+    const users = await User.find({ _id: req.params.id });
+    res.status(200).json(users);
   } catch (error) {
-    console.error(error)
-    res.status(400).json({message: error.message})
+    console.error(error);
+    res.status(400).json({ message: error.message });
   }
 }
 
 async function createUser(req, res) {
   try {
-    const doc = await User.create(req.body)
-    res.status(201).json({data: doc})
+    const doc = await User.create(req.body);
+    res.status(201).json({ data: doc });
   } catch (error) {
-    console.error(error)
-    res.status(400).json({message: error.message})
+    console.error(error);
+    res.status(400).json({ message: error.message });
   }
 }
 
 async function deleteUserById(req, res) {
-  
+  try {
+    const doc = await User.create(req.body);
+    res.status(201).json({ data: doc });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: error.message });
+  }
 }
 
-async function updateUserById(req, res) {
-  
-}
+async function updateUserById(req, res) {}
 
 module.exports = {
   getAllUsers,
   getUserById,
   createUser,
   deleteUserById,
-  updateUserById
-}
+  updateUserById,
+};
